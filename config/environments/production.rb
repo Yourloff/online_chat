762186111dd86d4e55a9aa23593d044ccf9a6e3f
Alexey <deploy@194-67-110-196.cloudvps.regruhosting.ru>
2022-07-28 08:47:49 +0300

    save prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
+   config.action_cable.mount_path = nil
+
+   config.action_cable.url = 'wss://194.67.110.196/cable'
+   config.action_cable.allowed_request_origins = [
+  'http://194.67.110.196/cable',
+  'http://194.67.110.196/cable'
+]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
